5c5ee89f2b493519e0afab9a77969fbe62b475da
Jakub Kozler <kozlerjakub@gmail.com>
2024-01-28 15:26:22 +0100

    simple promise

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -164,6 +164,34 @@ const whereAmI = async function (lat, lng) {
   }
 };
 
-whereAmI(52.508, 13.381);
+// whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lottery is happening!');
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN!');
+    } else {
+      reject(new Error('You did not WIN!'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// promise setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds.');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second.'));
